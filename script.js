0141f2c0b966d842db2c105263049e7e3cfaac6d
Artur Błażejczyk <arturb947@gmail.com>
2019-07-22 19:01:20 +0200

    add event to .wrapper in JS & jQuery

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // jQuerry
 
-$('.burger').on('click', function(){
-    $('.fas, aside').toggleClass('show');
-})
+/* $('.burger').on('click', function(){
+    $('.fas, aside, .wrapper').toggleClass('show');
+}) */
 
 
 // CLEAR JAVASCRIPT
 
-/* const burger = document.querySelector(".burger");
+const burger = document.querySelector(".burger");
 
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-times");
 const column = document.querySelector("aside");
+const wrapper = document.querySelector(".wrapper")
 
 burger.addEventListener('click', function(){
     iconBurger.classList.toggle('show');
     iconX.classList.toggle('show');
     column.classList.toggle('show');
-}) */
+    wrapper.classList.toggle('show');
+})
 
